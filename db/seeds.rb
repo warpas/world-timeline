acb73e4ac018e789271dfccd4ba78ecfb9b73e13
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-01-15 17:29:29 +0100

    Simplify working with a fresh DB - Add first 3 timeline_points to seeds - Change root route to timeline_points#index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+TimelinePoint.create(name: "Muslim siege of Byzantium", description: "emperor Leo III 717-718 source: History of Byzantium episode 69")
+TimelinePoint.create(name: "Rome's 1000th birthday", description: "248 AD Source: History of Rome podcast, episode 109")
+TimelinePoint.create(name: "Founding of Rome", description: "The traditional date for the founding of Rome is 753-04-21 BCE, following Marcus Terentius Varro Source: https://en.wikipedia.org/wiki/History_of_Rome")
